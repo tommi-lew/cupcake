0147d512b6841d5ad50d82f1bf6d4cc332271a83
Tommi Lew <lew.tommi@gmail.com>
2015-08-10 21:47:00 +0800

    Add GitHubService

---
+++ app/services/github_service.rb
@@ -0,0 +1,25 @@
+class GithubService
+  def self.update_pivotal_tracker_stories
+    pull_requests = self.get_pull_requests
+
+    PivotalTrackerStory.without_pull_requests.each do |story|
+      pull_request = pull_requests.find do |pr|
+        !!pr.title.index(story.tracker_id)
+      end
+
+      next if pull_request.nil?
+
+      story.update_attributes(pull_request_no: pull_request.number)
+    end
+  end
+
+  def self.get_pull_requests
+    self.github_client.pull_requests.list
+  end
+
+  private
+
+  def self.github_client
+    Github.new(user: ENV['GITHUB_OWNER'], repo: ENV['GITHUB_REPO'])
+  end
+end
---
+++ spec/services/github_service_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+describe GithubService do
+  describe '.update_pivotal_tracker_stories' do
+    it 'update pivotal tracker stories with pull request id' do
+      story1 = create(:pivotal_tracker_story)
+      story2 = create(:pivotal_tracker_story)
+
+      fake_pr1 = Object.new
+      stub(fake_pr1).title { "[##{story1.tracker_id}] #{story1.name}" }
+      stub(fake_pr1).number { 10 }
+
+      fake_pr2 = Object.new
+      stub(fake_pr2).title { "[##{story2.tracker_id}] #{story2.name}" }
+      stub(fake_pr2).number { 20 }
+
+      fake_pr3 = Object.new
+      stub(fake_pr3).title { "[#9] No existent story" }
+      stub(fake_pr3).number { 90 }
+
+      stub(GithubService).get_pull_requests { [fake_pr1, fake_pr2, fake_pr3] }
+
+      GithubService.update_pivotal_tracker_stories
+
+      story1.reload
+      story2.reload
+
+      expect(story1.pull_request_no).to eq(10)
+      expect(story2.pull_request_no).to eq(20)
+    end
+  end
+
+  describe '.get_pull_requests' do
+    it 'hits github api and return open pull requests' do
+      mock.instance_of(Github::Client).pull_requests.mock!.list { 'pull_requests' }
+
+      pull_requests = GithubService.get_pull_requests
+
+      expect(pull_requests).to eq 'pull_requests'
+    end
+  end
+
+  describe '.github_client' do
+    it 'returns a github client instance' do
+      expect(GithubService.github_client).to be_a Github::Client
+    end
+  end
+end
