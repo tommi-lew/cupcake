e9c59459e5bdeb94fe10b6cdc55befa22f21216f
Tommi Lew <lew.tommi@gmail.com>
2015-12-07 10:37:23 +0800

    Add User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151207020025) do
+ActiveRecord::Schema.define(version: 20151207023141) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,15 @@ ActiveRecord::Schema.define(version: 20151207020025) do
     t.datetime "updated_at",                    null: false
     t.text     "pt_owner_ids",     default: [],              array: true
     t.text     "pull_request_nos", default: [],              array: true
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "email"
+    t.integer  "pt_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
